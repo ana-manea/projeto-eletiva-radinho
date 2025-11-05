d50f219c4db1e03fe0114f101bfd597e7c93f32e
ana-manea <ana_manea@hotmail.com>
2025-11-05 12:02:59 -0300

    Redirecionamento ao entrar com senha

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
-function redirecionarHome() {
-    if (document.getElementById('entrar_senha').id === "entrar_senha") {
-        window.location.href = "assets/view/entrarSenha.html";
+function redirecionarSenha() {
+    if (document.getElementById('entrar_senha').id === 'entrar_senha') {
+        window.location.href = "../projeto-eletiva-radinho/assets/view/entrarSenha.html";
     }
 }
